bb2365c02972e684f6e1ea6c8479fe396f4f8960
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2025-03-24 11:45:45 +0100

    resources/building-bao-hypervisor/config.c: update config
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>

--- resources/building-bao-hypervisor/config.c
+++ resources/building-bao-hypervisor/config.c
@@ -1,45 +1,51 @@
 #include <config.h>
 
 struct config config = {
+    /**
+     * This defines an array of shared memory objects that may be associated
+     * with inter-partition communication objects in the VM platform definition
+     * below using the shared memory object ID, ie, its index in the list.
+     */
     .vmlist_size = 2,
     .vmlist = (struct vm_config[]) {
         {
             .image = VM_IMAGE_LOADED(0x00020000, 0x00020000, 0xD00),
-
             .entry = 0x00020000,
-
             .platform = {
                 .cpu_num = 1,
                 .region_num = 2,
                 .regions =  (struct vm_mem_region[]) {
                     {
                         .base = 0x20010000, //SRAM1
-                        .size = 0x8000
+                        .size = 0x7000
                     },
                     {
                         .base = 0x00020000,
                         .size = 0x10000
                     }
                 },
-
-                .dev_num = 1,
+                .dev_num = 2,
                 .devs =  (struct vm_dev_region[]) {
                     {
-                        /* Flexcomm Interface 1 (USART1) */
-                        .pa = 0x40087000,
-                        .va = 0x40087000,
+                        /* Flexcomm Interface 2 (USART2) */
+                        .pa = 0x40088000,
+                        .va = 0x40088000,
                         .size = 0x1000,
                         .interrupt_num = 1,
-                        .interrupts = (irqid_t[]) {15+15}
+                        .interrupts = (irqid_t[]) {16+16}
+                    },
+                    {
+                        /* SYSCON + IOCON */
+                        .pa = 0x40000000,
+                        .va = 0x40000000,
+                        .size = 0x2000,
                     },
                 },
             },
         },
         {
             .image = VM_IMAGE_LOADED(0x00040000, 0x00040000, 0xD00),
-
             .entry = 0x00040000,
-
             .platform = {
                 .cpu_num = 1,
                 .region_num = 2,
@@ -52,6 +58,23 @@ struct config config = {
                         .base = 0x00040000,
                         .size = 0x10000
                     }
+                },
+                .dev_num = 2,
+                .devs =  (struct vm_dev_region[]) {
+                    {
+                        /* Flexcomm Interface 3 (USART3) */
+                        .pa = 0x40089000,
+                        .va = 0x40089000,
+                        .size = 0x1000,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {17+16}
+                    },
+                    {
+                        /* SYSCON + IOCON */
+                        .pa = 0x40000000,
+                        .va = 0x40000000,
+                        .size = 0x2000,
+                    },
                 },
             },
         },
