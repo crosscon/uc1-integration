16bb0a8c23fd9c090ced4efebbcb2079cd407aa9
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2025-05-14 15:52:01 +0200

    infrastructure for PUF integration with 2x Zephyr VM
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ hello_world_vm0/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 3mdeb Sp z o.o.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World from Zephyr VM0 running on CROSSCON HV!\n");
+
+	return 0;
+}
