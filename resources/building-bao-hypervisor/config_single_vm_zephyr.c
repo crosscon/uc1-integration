611ff3f9fdcda8e02891b2931e72fdc4b6c0621a
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2025-05-29 12:47:43 +0200

    config_single_vm_zephyr.c: increase RAM for Zephyr VM
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- resources/building-bao-hypervisor/config_single_vm_zephyr.c
+++ resources/building-bao-hypervisor/config_single_vm_zephyr.c
@@ -8,25 +8,23 @@ struct vm_config zephyr = {
         .region_num = 2,
         .regions =  (struct vm_mem_region[]) {
             {
-                .base = 0x20030000, //SRAM1
-                .size = 0x10000
+                .base = 0x20021000, //SRAM1
+                .size = 0x1E000 /* 120 KB */
             },
             {
                 .base = 0x00048000,
-                .size = 0x28000
+                .size = 0x40000
             }
         },
         .dev_num = 4,
         .devs =  (struct vm_dev_region[]) {
             {
-                /* Flexcomm Interface 2 (USART2) */
-                /* AND */
-                /* Flexcomm Interface 3 (USART3) */
+                /* Flexcomm Interface 2 (USART2) + USART3 */
                 .pa = 0x40088000,
                 .va = 0x40088000,
                 .size = 0x2000,
                 .interrupt_num = 2,
-                .interrupts = (irqid_t[]) {16+16, 17+16}
+                .interrupts = (irqid_t[]) {16+16, 16+17}
             },
             {
                 /* SYSCON + IOCON + PINT + SPINT */
@@ -50,7 +48,7 @@ struct vm_config zephyr = {
         .ipc_num = 1,
         .ipcs = (struct ipc[]) {
             {
-                .base = 0x20017000,
+                .base = 0x20020000,
                 .size = 0x1000,
                 .shmem_id = 0,
                 .interrupt_num = 1,
@@ -65,7 +63,7 @@ struct config config = {
     CONFIG_HEADER
     .shmemlist_size = 1,
     .shmemlist = (struct shmem[]) {
-        [0] = {.base = 0x20017000, .size = 0x1000,},
+        [0] = {.base = 0x20020000, .size = 0x1000,},
     },
     .vmlist_size = 1,
     .vmlist = (struct vm_config*[]) {
