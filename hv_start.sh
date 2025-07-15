c10ded67dfa0aa0d0f44e0420bb18f64206dc64a
Mateusz Kusiak <mateusz.kusiak@3mdeb.com>
2025-07-15 16:25:48 +0200

    hv_start: load zephyr VMs
    Load zephyr VMs by default, this helps when debug is needed.
    
    Signed-off-by: Mateusz Kusiak <mateusz.kusiak@3mdeb.com>

--- hv_start.sh
+++ hv_start.sh
@@ -14,10 +14,19 @@ sleep 3
 
 # Run GDB commands non-interactively
 arm-none-eabi-gdb out/crossconhyp.elf \
+  -ex "set pagination off" \
+  -ex "set confirm off" \
   -ex "target remote :3333" \
   -ex "set \$pc=_reset_handler" \
+  -ex "set confirm on" \
   -ex "continue" \
+  -ex "ref" \
   -ex "quit"
+
+# Note, you can also load the zephyr debug symbols via following commands
+# -ex "add-symbol-file out/vm0.elf 0x00020000" \
+# -ex "add-symbol-file out/vm1.elf 0x00048000" \
+# Update the VM address and add these before "set confirm on"
 
 # After GDB exits, kill the gdbserver
 kill $GDBSERVER_PID
